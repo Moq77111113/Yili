562466fdc5bd136b4981a6aeaecf43e1584ff56e
Quentin Moessner <quentin.moessner@gmail.com>
2022-09-07 08:10:49 +0200

    Develop (#2)
    * style: responsiveness
    
    * feat(layout): add easter egg
    
    Co-authored-by: Quentin Moessner <q.moessner@yamay.app>

--- src/lib/components/MovieCard.svelte
+++ src/lib/components/MovieCard.svelte
@@ -5,16 +5,17 @@
 </script>
 
 <div
-	class="card w-42 bg-base-100 shadow-xl flex flex-col rounded-3xl  justify-between
+	class="card w-42 bg-base-100 shadow-xl rounded-3xl
 	hover:rounded-xl hover:shadow-gray-600 cursor-pointer
 	transition-all duration-300 ease-linear"
 >
-	<a sveltekit:prefetch href={`/movies/${movie.id}`}>
+	<a sveltekit:prefetch href={`/movies/${movie.id}`} class="flex flex-col justify-between h-full">
 		<figure>
 			<img
 				src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
 				alt={movie.title}
-				class="text-xs h-[300px] lg:h-[500px]"
+				class="text-xs h-[300px] md:h-[400px] lg:h-[450px] 2xl:h-[500px]
+				 transition-all duration-300 ease-linear"
 			/>
 		</figure>
 		<div class="card-body">
